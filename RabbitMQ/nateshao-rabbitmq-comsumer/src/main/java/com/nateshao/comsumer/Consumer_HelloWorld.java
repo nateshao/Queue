cb99a358ae84adf3d02021433afd0e3a5f79a02d
nateshao <shaotongjie@gmail.com>
2020-09-18 22:06:04 +0800

    Consumer_HelloWorld

---
+++ RabbitMQ/nateshao-rabbitmq-comsumer/src/main/java/com/nateshao/comsumer/Consumer_HelloWorld.java
@@ -0,0 +1,13 @@
+package com.nateshao.comsumer;
+
+/**
+ * @date Created by 邵桐杰 on 2020/9/18 22:05
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+public class Consumer_HelloWorld {
+
+}
