207ef6d4419e76c1c31210d8af7a5964c150d25a
nateshao <shaotongjie@gmail.com>
2020-09-18 18:48:59 +0800

    a

---
+++ RabbitMQ/nateshao-rabbitmq-comsumer/src/main/java/com/nateshao/NateshaoRabbitmqComsumerApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoRabbitmqComsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoRabbitmqComsumerApplication.class, args);
+    }
+
+}
